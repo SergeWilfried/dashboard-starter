a56eda1989d31edc9841fa8f8990eaa3717efdad
serge <sergewilfried@outlook.fr>
2024-02-18 17:01:19 +0000

    fix(page.tsx): update text to 'Sign In' and 'Enter your email below to sign in' feat(page.tsx): add new 'SignUp' page with registration form and related components feat(user-auth-form.tsx): add UserRegistrationAuthForm for user registration feat(user-auth-form.tsx): add registrationFormSchema for user registration form validation feat(user-auth-form.tsx): add UserAuthForm for user authentication feat(github-auth-button.tsx): rename GoogleSignInButton to GithubSignInButton

--- app/(auth)/(signup)/page.tsx
+++ app/(auth)/(signup)/page.tsx
@@ -1,15 +1,15 @@
 import { Metadata } from "next";
 import Link from "next/link";
-import { UserAuthForm } from "@/components/forms/user-auth-form";
+import { UserRegistrationAuthForm } from "@/components/forms/user-auth-form";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
-  title: "Authentication",
+  title: "Sign Up",
   description: "Authentication forms built using the components.",
 };
 
-export default function AuthenticationPage() {
+export default function SignUp() {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <Link
@@ -19,7 +19,7 @@ export default function AuthenticationPage() {
           "absolute right-4 hidden top-4 md:right-8 md:top-8",
         )}
       >
-        Login
+        Register
       </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
@@ -53,13 +53,13 @@ export default function AuthenticationPage() {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Sign In
+              Create an account
             </h1>
             <p className="text-sm text-muted-foreground">
-              Enter your email below to sign in
+              Enter your email and password to create your account
             </p>
           </div>
-          <UserAuthForm />
+          <UserRegistrationAuthForm />
           <p className="px-8 text-center text-sm text-muted-foreground">
             By clicking continue, you agree to our{" "}
             <Link
--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -21,9 +21,101 @@ const formSchema = z.object({
   email: z.string().email({ message: "Enter a valid email address" }),
 });
 
+const registrationFormSchema = z.object({
+  email: z.string().email({ message: "Enter a valid email address" }),
+  password: z.string()
+});
+
 type UserFormValue = z.infer<typeof formSchema>;
 
-export default function UserAuthForm() {
+type RegisterUserFormValue = z.infer<typeof registrationFormSchema>;
+
+export function UserRegistrationAuthForm() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  const [loading, setLoading] = useState(false);
+  const defaultValues = {
+    email: "demo@gmail.com",
+    password: 'password'
+  };
+  const form = useForm<RegisterUserFormValue>({
+    resolver: zodResolver(registrationFormSchema),
+    defaultValues,
+  });
+
+  const onSubmit = async (data: RegisterUserFormValue) => {
+    signIn("credentials", {
+      email: data.email,
+      callbackUrl: callbackUrl ?? "/dashboard",
+    });
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-2 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button disabled={loading} className="ml-auto w-full" type="submit">
+            Continue With Email and Password
+          </Button>
+        </form>
+      </Form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <GoogleSignInButton />
+    </>
+  );
+}
+
+export function UserAuthForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
   const [loading, setLoading] = useState(false);
